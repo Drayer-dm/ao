97f7ed9e92e0897b8e5971729a4af27858208555
Drayer <drayeryoncley11@gmail.com>
2026-06-29 17:21:29 -0400

    YESYESYES

---
+++ keyboardbackend/src/main/java/com/example/keyboardbackend/controllers/HealthController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "API de KeyboardPage funcionando al 100%";
+    }
+}
