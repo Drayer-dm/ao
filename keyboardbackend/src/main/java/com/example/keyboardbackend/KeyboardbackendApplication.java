ff2b1ad35eb1266840fe4bd09a1a7d74c813234f
Drayer <drayeryoncley11@gmail.com>
2026-06-29 17:20:08 -0400

    ahora si

---
+++ keyboardbackend/src/main/java/com/example/keyboardbackend/KeyboardbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.keyboardbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyboardbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyboardbackendApplication.class, args);
+	}
+
+}
---
+++ keyboardbackend/src/test/java/com/example/keyboardbackend/KeyboardbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keyboardbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyboardbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
