bf0a9ae902dfec62d8e19fe577588161b67971f4
Drayer Yoncley Duran Marquez <drayeryoncley11@gmail.com>
2026-07-04 15:18:20 -0400

    cambios de paquetes de jakarta para evitar sobrecarga en el sistema

--- keyboardbackend/src/main/java/com/example/keyboardbackend/model/AtributoVariante.java
+++ keyboardbackend/src/main/java/com/example/keyboardbackend/model/AtributoVariante.java
@@ -1,6 +1,12 @@
 package com.example.keyboardbackend.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
--- keyboardbackend/src/main/java/com/example/keyboardbackend/model/AtributoVarianteId.java
+++ keyboardbackend/src/main/java/com/example/keyboardbackend/model/AtributoVarianteId.java
@@ -1,6 +1,6 @@
 package com.example.keyboardbackend.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Embeddable;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
